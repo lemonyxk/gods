50aa36a2e1e3c0bdf7b52fc681c84d7318933950
lemo <lemo@gamstudio.net>
2022-03-25 21:22:20 +0800

    1.singlylinkedlist generics are supported.

--- lists/singlylinkedlist/serialization.go
+++ lists/singlylinkedlist/serialization.go
@@ -6,22 +6,23 @@ package singlylinkedlist
 
 import (
 	"encoding/json"
-	"github.com/emirpasic/gods/containers"
+
+	"github.com/lemonyxk/gods/containers"
 )
 
-func assertSerializationImplementation() {
-	var _ containers.JSONSerializer = (*List)(nil)
-	var _ containers.JSONDeserializer = (*List)(nil)
+func assertSerializationImplementation[T comparable]() {
+	var _ containers.JSONSerializer = (*List[T])(nil)
+	var _ containers.JSONDeserializer = (*List[T])(nil)
 }
 
 // ToJSON outputs the JSON representation of list's elements.
-func (list *List) ToJSON() ([]byte, error) {
+func (list *List[T]) ToJSON() ([]byte, error) {
 	return json.Marshal(list.Values())
 }
 
 // FromJSON populates list's elements from the input JSON representation.
-func (list *List) FromJSON(data []byte) error {
-	elements := []interface{}{}
+func (list *List[T]) FromJSON(data []byte) error {
+	elements := []T{}
 	err := json.Unmarshal(data, &elements)
 	if err == nil {
 		list.Clear()
